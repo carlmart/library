730f89ce46ec4ae6fcf6ee59eb10ae11e2a77882
Carlos Martinez <marti149@gmail.com>
2011-02-21 20:54:39 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Books::Application.config.secret_token = '968c8ca092881f94deb03860e8eb7d72d2202c899e013a4ee672b95030827cf24b744990d5ff75cc091c3c34add804654dd084869b7b87ecdf4864a29f2fac60'
---
+++ db/migrate/20110220163810_create_books.rb
@@ -0,0 +1,15 @@
+class CreateBooks < ActiveRecord::Migration
+  def self.up
+    create_table :books do |t|
+      t.string :book_title
+      t.string :subject
+      t.integer :author_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :books
+  end
+end
